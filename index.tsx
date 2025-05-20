65966c9d0cff5a15cefe3e53d70283887cd6106e
luckynocode <151090847+jll211@users.noreply.github.com>
2025-05-21 00:57:31 +0200

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ComponentGallery from "./ComponentGallery";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ComponentGallery />);
